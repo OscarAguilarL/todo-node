9fc1c4ccbd634bcf25a45605f856fda621e80767
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-03-12 17:43:07 -0600

    Crear y listar tareas

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('colors');
 
-const { inquirerMenu, pausa } = require('./helpers/inquirer');
-const Tarea = require('./models/tarea');
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer');
 const Tareas = require('./models/tareas');
 
 console.clear();
@@ -10,14 +9,34 @@ const main = async () => {
     console.log('Hola mundo');
 
     let opt = '';
+    const tareas = new Tareas();
 
     do {
-        // opt = await inquirerMenu();
-        // console.log({ opt });
+        opt = await inquirerMenu();
 
-        const tareas = new Tareas()
-        const tarea = new Tarea('Comprar comida');
-        console.log(tareas);
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Descripción: ');
+                tareas.crearTarea(desc);
+                break;
+            case '2':
+                console.log(tareas._listado);
+                break;
+            case '3':
+                // Crear opción
+                break;
+            case '4':
+                // Crear opción
+                break;
+            case '5':
+                // Crear opción
+                break;
+            case '6':
+                // Crear opción
+                break;
+            default:
+                break;
+        }
 
         await pausa();
     } while (opt !== '0');
